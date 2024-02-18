6dd12705d4f27ad13d03e67419cfcd7578d4d7b8
Isaque Claudino <isaque.santos@after.sale>
2024-02-18 18:19:47 -0300

    chore: removed code

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,6 @@ import App from "./App";
 import "./styles/input.css";
 import "./styles/reset.css";
 import "./socket";
-import { axios_instance } from "./axios_instance";
-
-axios_instance.get("/").then((res) => console.log(res.data));
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
